69e4be84f2f27e78492afc8b0a1724e858a856c3
Marcel Medina <marcel.net@gmail.com>
2023-05-27 17:14:25 +1200

    azure-functions-status-codes

---
+++ AzureFunctionsStatusCodes/FunctionHttpResponses.cs
@@ -0,0 +1,57 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctionsStatusCodes
+{
+    public class FunctionHttpResponses
+    {
+        private readonly ILogger _logger;
+
+        public FunctionHttpResponses(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<FunctionHttpResponses>();
+        }
+
+        [Function(nameof(OkHttpStatusCode))]
+        public HttpResponseData OkHttpStatusCode([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+
+        [Function(nameof(BadRequestHttpStatusCode))]
+        public HttpResponseData BadRequestHttpStatusCode([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse();
+            response.WriteString("Welcome to Azure Functions!");
+            response.StatusCode = HttpStatusCode.BadRequest;
+
+            return response;
+        }
+
+        [Function(nameof(OkStatusCode))]
+        public IActionResult OkStatusCode([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new OkObjectResult("Welcome to Azure Functions!");
+        }
+
+        [Function(nameof(AlwaysHttp200StatusCode))]
+        public IActionResult AlwaysHttp200StatusCode([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            return new BadRequestObjectResult("Welcome to Azure Functions!");
+        }
+    }
+}
